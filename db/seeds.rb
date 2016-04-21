294f311668df20f8cd8d7165f09f83574ceccb70
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-04-21 17:49:22 -0400

    troubleshooting

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,14 +17,15 @@ require 'json'
 
 
 
-@dataTable = CSV.table(data_original, :skip_blanks => true, :skip_lines => /^(?:,\s*)+$/)
+@dataTable = CSV.table(data_original, :skip_blanks => true)
 
 # @dataTable = CSV.parse(data_original, headers: true, skip_blanks: true).delete_if { |row| row.to_hash.values.all?(&:blank?) }
 
 CSV.open("#{Rails.root}/db/data_only_boundaries.csv", "w") do |f|
-  @dataTable[:boundaries].each do |x|
+  @dataTable[:boundaries][0..75].each do |x|
     f << [x]
   end
+  binding.pry
 end
 
 CSV.open("#{Rails.root}/db/data_without_boundaries.csv", "w") do |csv|
@@ -44,53 +45,64 @@ end
 #   end
 # end
 
-boundaries = CSV.read("#{Rails.root}/db/data_only_boundaries.csv")
+# boundaries = CSV.read("#{Rails.root}/db/data_only_boundaries.csv")
+boundaries = []
+CSV.foreach(data_boundaries, :skip_blanks => true, :skip_lines => /^(?:,\s*)+$/) do |row|
+  boundaries << row[0]
+end
+
+boundaries.map!{|row| eval(row)}
+
+# binding.pry
 # p boundaries[0].to_json
 # p data_without_boundaries.count
 
-p eval(boundaries[20][0]).to_json
-
-boundaries.map!{|row| eval(row[0].gsub("=>", ":").gsub("\"", ""))}
+# p eval(boundaries[20][0]).to_json
+# p boundaries
+# p boundaries.map{|row| eval(row[0])}
+# p boundaries.map!{|row| eval(row[0].gsub("=>", ":").gsub("\"", ""))}
 
 
 CSV.foreach(data_without_boundaries, :skip_blanks => true, :headers => true, :skip_lines => /^(?:,\s*)+$/ ) do |row|
-    # puts $.
-    # p row
-    # p row
-    # (0..13).step do |i|
-    #   puts row[i]
-    # end
-    # p eval(row[14]).to_json
-    # p row[14].to_h if row[14] != "{}"
-    # p row[14].gsub(/(^\"|\"?$)/, '').to_a.to_h
-    # p CSV.parse(row[14])
+  #   puts $.
+  #   p row
+  #   p row
+  #   (0..13).step do |i|
+  #     puts row[i]
+  #   end
+  #   p eval(row[14]).to_json
+  #   p row[14].to_h if row[14] != "{}"
+  #   p row[14].gsub(/(^\"|\"?$)/, '').to_a.to_h
+  #   p CSV.parse(row[14])
+  #
+  #   p row[14].gsub("", "").scan(/{(.*)}/)
+  #   p JSON.parse(row[14], :quirks_mode => true)
+  #
+  # p boundaries[$.]
 
-    # p row[14].gsub("", "").scan(/{(.*)}/)
-    # p JSON.parse(row[14], :quirks_mode => true)
 
-  p boundaries[$.]
-
-
-  # School.create!({
-  #                  :name => row[0],
-  #                  :school_type => row[1],
-  #                  :mingrade => row[2].to_i,
-  #                  :maxgrade => row[3].to_i,
-  #                  :phone => row[4],
-  #                  :website => row[5],
-  #                  :language => row[6],
-  #                  :created_at => row[7],
-  #                  :updated_at => row[8],
-  #                  :is_catholic => row[9],
-  #                  :level => row[10][(/[A-Z]+/i)],
-  #                  :street => row[11],
-  #                  :postal_code => row[12],
-  #                  :latitude => row[13].to_i,
-  #                  :boundaries => boundaries[$.],
-  #                  :slug => row[0].gsub(/[^\s\w]/, '').gsub(/\s/, '-').downcase
-  #                })
-    # puts "Row added!"
+  School.create!({
+                   :name => row[0],
+                   :school_type => row[1],
+                   :mingrade => row[2].to_i,
+                   :maxgrade => row[3].to_i,
+                   :phone => row[4],
+                   :website => row[5],
+                   :language => row[6],
+                   :created_at => row[7],
+                   :updated_at => row[8],
+                   :is_catholic => row[9],
+                   :level => row[10][(/[A-Z]+/i)],
+                   :street => row[11],
+                   :postal_code => row[12],
+                   :latitude => row[13].to_i,
+                   :boundaries => boundaries[$.],
+                   :slug => row[0].gsub(/[^\s\w]/, '').gsub(/\s/, '-').downcase
+                 })
+    puts "Row added!"
 end
+
+binding.pry
 
 
 
