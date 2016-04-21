9631583152f6c729f4a323cf96c4cea5af601d32
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-04-20 21:15:59 -0400

    generate schools controller with index and show actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'schools/index'
+
+  get 'schools/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
