1fb86372d7c367b9188c90ebb2747e37640ff6fa
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-04-24 14:25:03 -0400

    set up rails_helper.rb and spec_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,10 +3,12 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
 require 'spec_helper'
 require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
+require 'support/factory_girl.rb'
 
+# Add additional requires below this line. Rails is not loaded until this point!
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -27,13 +29,21 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+
+
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
+
+  #NOTE - Set to false because we are going to use factories not fixtures.
+  #NOTE - I realized I don't fully understand what this is doing.
   config.use_transactional_fixtures = true
+
+  # NOTE - review different available types
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,16 +7,27 @@
 # Given that it is always loaded, you are encouraged to keep this file as
 # light-weight as possible. Requiring heavyweight dependencies from this file
 # will add to the boot time of your test suite on EVERY test run, even for an
-# individual file that may not need all of that loaded. Instead, consider making
-# a separate helper file that requires the additional dependencies and performs
-# the additional setup, and require it from the spec files that actually need
-# it.
-#
+# individual file that may not need all of that loaded.
+
+
+# NOTE - Create a helper file that requires factory girl dependencies and performs any related set up?
+
+# Instead, consider making a separate helper file that requires the additional dependencies and performs
+# the additional setup, and require it from the spec files that actually need it.
+
 # The `.rspec` file also contains a few flags that are not defaults but that
 # users commonly want.
-#
+
+
+
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+
+
+  # Q: What is rspec-expectations?
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -28,31 +39,50 @@ RSpec.configure do |config|
     #     # => "be bigger than 2 and smaller than 4"
     # ...rather than:
     #     # => "be bigger than 2"
+
+    #Q: What are custom matchers?
+    #Q: What are helper methods defined using chain? What do they do? Why are they useful?
+
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
+
+  #Q: What is rspec-mocks?
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
+
+    #Q: What is mocking and stubbing?
+
     # Prevents you from mocking or stubbing a method that does not exist on
     # a real object. This is generally recommended, and will default to
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
   end
 
-# The settings below are suggested to provide a good initial experience
-# with RSpec, but feel free to customize to your heart's content.
-=begin
+  # The settings below are suggested to provide a good initial experience
+  # with RSpec, but feel free to customize to your heart's content.
+
   # These two settings work together to allow you to limit a spec run
   # to individual examples or groups you care about by tagging them with
   # `:focus` metadata. When nothing is tagged with `:focus`, all examples
   # get run.
+
+  # NOTE - This setting allows you to run only specified tests.
+  #Q: What does config.run_all_when_everything_filtered? do?
+
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
 
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options. We recommend
   # you configure your source control system to ignore this file.
+
+  #Q: What are --only-failures and --next-failure CLI options?
+  #Q: What is CLI exactly?
+
+  #NOTE - Suggest we add "spec/examples.txt" to git ignore?
+
   config.example_status_persistence_file_path = "spec/examples.txt"
 
   # Limits the available syntax to the non-monkey patched syntax that is
@@ -60,6 +90,9 @@ RSpec.configure do |config|
   #   - http://rspec.info/blog/2012/06/rspecs-new-expectation-syntax/
   #   - http://www.teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
   #   - http://rspec.info/blog/2014/05/notable-changes-in-rspec-3/#zero-monkey-patching-mode
+
+  #Q: What is monkey patched syntax?
+
   config.disable_monkey_patching!
 
   # Many RSpec users commonly either run the entire suite or an individual
@@ -81,12 +114,20 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
+
+  #NOTE - by default rspec runs all tests randomly.
+
   config.order = :random
 
   # Seed global randomization in this process using the `--seed` CLI option.
   # Setting this allows you to use `--seed` to deterministically reproduce
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
+
+  #Q: What does --seed do exactly?
+  #Q: Seed global randomization?
+  #Q: What does the following line of code do exactly?
+
   Kernel.srand config.seed
-=end
+
 end
