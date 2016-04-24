79eefd973413b7bbed8d04ef58f779e31111c92c
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-04-24 17:19:37 -0400

    define school factory and start school_spec.rb

--- spec/factories/schools.rb
+++ spec/factories/schools.rb
@@ -1,5 +1,22 @@
 FactoryGirl.define do
+
   factory :school do
-    
+    name
+    school_type
+    mingrade
+    maxgrade
+    phone
+    website
+    language
+    created_at
+    updated_at
+    is_catholic
+    level
+    street
+    postal_code
+    latitude
+    slug
+    boundaries
   end
+
 end
